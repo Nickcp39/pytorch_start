97cd27913247c6f278a0ebf8014a994338f0570d
Yanda Cheng <yandache@buffalo.edu>
2022-02-23 15:34:40 -0500

    现在第一次代码写完了， 但是在图像读取的结果有些问题， 我可以看到 dataset的数据集的存在，但是不能img function直接读取imge 和 label， 有一点点奇怪， 但是先这样了， 直接进行下一步， 然后从github上拿他的代码继续进行测试

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ torch.cuda.is_available()
 print(torch.cuda.is_available())
 
 print(dir(torch))
-"这一行print torch cuda 中的独立工作项"
+#这一行print torch cuda 中的独立工作项"
 print(dir(torch.cuda))
-"介绍 help， 查看官方解释文档 功能对于 cuda的作用， 可以发现return CUDA 可以使用"
+#介绍 help， 查看官方解释文档 功能对于 cuda的作用， 可以发现return CUDA 可以使用"
 help(torch.cuda.is_available)
 
 from torch.utils.data import Dataset
@@ -21,7 +21,41 @@ import os
 
 class MyData(Dataset):
 
-    def __init__(self):
+    def __init__(self, root_dir, label_dir):
+        self.root_dir = root_dir
+        self.label_dir = label_dir
+        self.path = os.path.join(self.root_dir, self.label_dir)
+        self.img_path = os.listdir(self.path)  #拿到图片中所有的地址
 
     def __getitem__(self, idx):
+        img_name = self.image[idx]
+        img_item_path = os.path.join(self.root_dir, self.label_dir, img_name)
+        img = Image.open(img_item_path)
+        label = self.label_dir
 
+        return img, label
+
+    def __len__(self):
+        return (len(self.img_path))
+
+
+
+dir_path = "C:\\Users\\nickc\PycharmProjects\\buffaloTestProject\\dataset\\hymenoptera_data\\hymenoptera_data\\train\\ants"
+img_path_list = os.listdir(dir_path)
+
+# root_dir =  "C:\\Users\\nickc\PycharmProjects\\buffaloTestProject\\dataset\\hymenoptera_data\\hymenoptera_data\\train"
+
+root_dir = "./dataset/hymenoptera_data/hymenoptera_data/train"
+
+
+ants_label_dir = "ants"
+bees_label_dir = "bees"
+ants_dataset = MyData(root_dir, ants_label_dir)
+bees_dataset = MyData(root_dir, bees_label_dir)
+
+train_dataset = ants_dataset + bees_dataset
+
+
+img, label  =  ants_dataset[11]
+img.shows()
+ants_dataset[123].img.shows()
